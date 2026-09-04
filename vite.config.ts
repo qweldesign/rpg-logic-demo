ee38288b9ab961dde6e5c6b1bbd03ded6edfaab2
Taigo Ito <qwel.design@gmail.com>
2026-09-05 00:00:00 +0900

    release: v0.0.0 初期構成

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/demo/',
+  base: '/demo/00/',
   build: {
     rollupOptions: {
     output: {
