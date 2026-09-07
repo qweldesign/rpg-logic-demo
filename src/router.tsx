472be1cd3cd04eb9a52c4ff166ad1bd859b7b9a7
Taigo Ito <qwel.design@gmail.com>
2026-09-07 20:39:13 +0900

    feat: 編成画面UIの初期構成

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import Entrance from './parts/Entrance'
 import Docs from './parts/Docs'
+import Setup from './parts/Setup'
 import { docsLoader } from './docs/docsLoader'
 
 export const router = createBrowserRouter(
@@ -15,6 +16,10 @@ export const router = createBrowserRouter(
       { path: 'docs', children: [
         { index: true, element: <Docs />, loader: docsLoader },
         { path: ':docsId', element: <Docs />, loader: docsLoader }
+      ]},
+      { path: 'setup', children: [
+        { index: true, element: <Setup /> },
+        { path: ':uid', element: <Setup /> }
       ]}
     ]
   }],
