fd3ae2be54b59b87673c89ffe3b6969bd63459c3
Taigo Ito <qwel.design@gmail.com>
2026-09-07 19:11:58 +0900

    release: v0.3.0 キャラクタの実装

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/demo/',
+  base: '/demo/03/',
   build: {
     rollupOptions: {
     output: {
