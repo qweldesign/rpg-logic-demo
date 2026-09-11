449feadfeff32090f2bc33a55dafa90b7aa02541
Taigo Ito <qwel.design@gmail.com>
2026-09-11 19:26:54 +0900

    feat: 必要筋力による装備制限を適用

--- src/parts/Setup/Edit/EquipmentsSetting.tsx
+++ src/parts/Setup/Edit/EquipmentsSetting.tsx
@@ -2,7 +2,6 @@
 
 import { type Dispatch } from 'react'
 import { type State, type Action } from '.'
-import { WEAPON_KEYS, WEAPONS, SHIELD_KEYS, SHIELDS, ARMOR_KEYS, ARMORS } from '../../../domains/Character/Equipments'
 
 function EquipmentsSetting(
   { isNew, state, dispatch, calcGold }:{
@@ -28,25 +27,27 @@ function EquipmentsSetting(
       <div>
       <h5>残り所持金: <span className={calcGold(state, false) < 0 ? 'text-red-600 font-bold' : 'font-bold'}>{calcGold(state, false)} 金</span></h5>
         <label className="inline-block w-24 sm:text-right">武器: </label>
-        <select className="w-72 m-6 px-3 text-left" value={state.equips.weapon.name} onChange={(e) => onChangeEquip('weapon', e.target.value)}>
-          {WEAPON_KEYS.map((key, i) => (
-            <option key={i} value={key}>{key} | 性能:{key !== '装備無し' ? WEAPONS[key].dmgBase : 0}</option>
+        <select className="w-96 m-6 px-3 text-left" value={state.equips.weapon.name} onChange={(e) => onChangeEquip('weapon', e.target.value)}>
+          <option value="装備無し">装備無し</option>
+          {state.weaponList.map(([key, weapon], i) => key !== '装備無し' && (
+            <option key={i} value={key}>{key} | 性能:{weapon.dmgBase} ({weapon.dmgType === 2 ? '刺' : weapon.dmgType === 1 ? '切' : '叩'}) | {weapon.twoHanded ? '両手' : '片手'} | {weapon.gold}金</option>
           ))}
         </select>
       </div>
       <div>
         <label className="inline-block w-24 sm:text-right">盾: </label>
-        <select className="w-72 m-6 px-3 text-left" value={state.equips.shield?.name ?? ''} onChange={(e) => onChangeEquip('shield', e.target.value)}>
-          {SHIELD_KEYS.map((key, i) => (
-            <option key={i} value={key}>{key} | 性能:{SHIELDS[key].size * 2}</option>
+        <select className="w-96 m-6 px-3 text-left" value={state.equips.shield?.name ?? ''} onChange={(e) => onChangeEquip('shield', e.target.value)}>
+          <option value="装備無し">装備無し</option>
+          {state.shieldList.map(([key, shield], i) => key !== '装備無し' && (
+            <option key={i} value={key}>{key} | 性能:{shield.size * 2} | {shield.gold}金</option>
           ))}
         </select>
       </div>
       <div>
         <label className="inline-block w-24 sm:text-right">服・鎧: </label>
-        <select className="w-72 m-6 px-3 text-left" value={state.equips.armor.name} onChange={(e) => onChangeEquip('armor', e.target.value)}>
-          {ARMOR_KEYS.map((key, i) => (
-            <option key={i} value={key}>{key} | 性能:{key !== '服' ? ARMORS[key].dr : 0}</option>
+        <select className="w-96 m-6 px-3 text-left" value={state.equips.armor.name} onChange={(e) => onChangeEquip('armor', e.target.value)}>
+          {state.armorList.map(([key, armor], i) => (
+            <option key={i} value={key}>{key} | 性能:{key !== '服' ? armor.dr : 0} | {armor.gold}金</option>
           ))}
         </select>
       </div>
