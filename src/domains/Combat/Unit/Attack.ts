d40b76cd6bad877acca22cd8450b2a2c3b8c4beb
Taigo Ito <qwel.design@gmail.com>
2026-09-12 11:05:49 +0900

    feat: 攻撃/防御 (CombatAttack / CombatDefense) の定義

---
+++ src/domains/Combat/Unit/Attack.ts
@@ -0,0 +1,52 @@
+// src/domains/Combat/Unit/Attack.ts
+
+import { type Dmg } from '../../Character'
+import { type CombatUnitModel as UnitModel } from '.'
+
+const DMG_RATE = [1, 1.5, 2]
+
+export class CombatAttack {
+  // 設定値
+  public name: string
+  public level: number // 大盾による修正込み
+  public dmg: Dmg
+  public dmgName: string
+  public needsReady: boolean
+  // 状態値
+  public ready: boolean // 準備の可否
+
+  constructor(model: UnitModel) {
+    this.name = model.equipments.weapon.name
+    this.level = model.level
+    this.dmg = model.equipments.getDmg(model.dmgMod)
+    this.dmgName = model.equipments.getDmgName(model.dmgMod)
+    this.needsReady = model.equipments.weapon.ready
+    this.ready = true
+  }
+
+  // 攻撃 (命中判定) の目標値を取得
+  // 各種自身の状況による修正値 (バフ, デバフ) を含める
+  // 各種戦闘の状況による修正値 (全力攻撃オプションによる修正) を含めない
+  get target(): number {
+    return Math.max(4, this.level)
+  }
+
+  // 攻撃 (命中判定) の目標値を取得
+  // 各種状況による修正値 (バフ, デバフ, 全力攻撃オプションによる修正) を含める
+  getTarget(): number {
+    return Math.max(4, this.target)
+  }
+
+  // 攻撃型によるダメージ倍率を取得
+  getDmgRate(): number {
+    return DMG_RATE[this.dmg.dmgType]
+  }
+
+  // 攻撃 (ダメージ判定) の期待値を取得
+  getExpectedDmg(dr: number = 0, isChain: boolean = false) {
+    const count = this.dmg.dmgDice
+    const mod = this.dmg.dmgMod - (this.dmg.dmgType === 2 && isChain ? Math.floor(dr / 2) : dr)
+    const rate = this.getDmgRate()
+    return Math.max(0, Math.floor((count * 3.5 + mod) * rate))
+  }
+}
