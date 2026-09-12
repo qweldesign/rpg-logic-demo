3a98454709409b5b5ccbc25435266d292b69944a
Taigo Ito <qwel.design@gmail.com>
2026-09-12 11:06:24 +0900

    feat: 戦闘場面に用いるコンポーネントの用意

--- src/parts/Combat/index.tsx
+++ src/parts/Combat/index.tsx
@@ -1,6 +1,10 @@
 // src/parts/Combat/index.tsx
 
 import { useRef, useState, useEffect } from 'react'
+import Formation from './Formation'
+import Action from './Action'
+import Summary from './Summary'
+import Timeline from './Timeline'
 import { SaveData } from '../../domains/SaveData'
 import { Combat as State } from '../../domains/Combat'
 import { playerSetup } from '../../domains/Combat/Formation/player'
@@ -43,7 +47,28 @@ function Combat() {
 
   return (
     <div className="p-6">
-      <p>In development...</p>
+      <div className="table-wrapper">
+        {stateRef.current && (
+          <div className="row justify-center min-w-lg lg:min-w-5xl">
+            <div id="formation" className="relative order-1 w-lg h-48 p-3 bg-white/15">
+              <h3 className="m-0 border-0 font-serif text-sm">Formation</h3>
+              <Formation />
+            </div>
+            <div id="summary" className="relative order-2 lg:order-3 w-lg h-96 p-3 bg-white/30">
+              <h3 className="m-0 border-0 font-serif text-sm">Summary</h3>
+              <Summary />
+            </div>
+            <div id="action" className="relative order-3 lg:order-2 w-lg h-48 p-3 bg-white/15 lg:bg-white/30">
+              <h3 className="m-0 border-0 font-serif text-sm">Action</h3>
+              <Action />
+            </div>
+            <div id="log" className="relative order-4 w-lg h-96 bg-white/30 p-3 lg:bg-white/15">
+              <h3 className="m-0 border-0 font-serif text-sm">Log</h3>
+              <Timeline />
+            </div>
+          </div> 
+        )}
+      </div>
     </div>
   )
 }
