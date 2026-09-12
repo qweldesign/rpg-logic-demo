735d6a4ea5dbfad1caaeb2077b2a593819badcec
Taigo Ito <qwel.design@gmail.com>
2026-09-12 13:02:56 +0900

    feat: 攻撃の実装過程 (ログ出力まで / ダメージ効果未実装)

--- src/domains/Combat/Log.tsx
+++ src/domains/Combat/Log.tsx
@@ -2,7 +2,7 @@
 
 import { type ReactNode } from 'react'
 import { CombatUnit as Unit } from './Unit'
-import { ACTION_LABELS, POSITION_LABELS, type ActionRequest } from './Action'
+import { type Judge, ACTION_LABELS, POSITION_LABELS, type ActionRequest, type ActionResult } from './Action'
 
 let count = 0
 
@@ -21,17 +21,17 @@ export class CombatLog {
     this.messages = [[firstMessage]]
   }
 
-  // Action コンポーネントで ActionRequet を受け取り, ラベルと結果ログを生成する
-  receiveResults(request: ActionRequest) {
-    this.label = this.createLabel(request)
-    this.messages.push(this.createMessages(request))
+  // Action コンポーネントで ActionRequest と ActionResult[] を受け取り, ラベルと結果ログを生成する
+  receiveResults(request: ActionRequest, results: ActionResult[] = []) {
+    this.label = this.createLabel(request, results)
+    this.messages.push(this.createMessages(request, results))
   }
 
   // ラベル生成 (Summary履歴用)
-  private createLabel(request: ActionRequest): string {
+  private createLabel(request: ActionRequest, results: ActionResult[]): string {
     switch (request.key) {
       case 'attack':
-        return `${ACTION_LABELS[request.key]}`
+        return `${ACTION_LABELS[request.key]}:${this.createAttackResultLabel(request, results)}`
 
       case 'move':
         return `${ACTION_LABELS[request.key]}:${POSITION_LABELS[request.options.position]}`
@@ -41,24 +41,72 @@ export class CombatLog {
     }
   }
 
+  // 攻撃の成否ラベルを生成
+  // 攻撃(成功) → 防御(失敗) → ダメージ(貫通) の場合のみ「成功」を返す
+  private createAttackResultLabel(request: ActionRequest, results: ActionResult[]): string {
+    if (request.key !== 'attack') return ''
+    let success = false
+    results.forEach(result => {
+      switch (result.type) {
+        case 'attack':
+          success = result.judge.success
+          break
+        case 'defense':
+          success = !result.judge.success
+          break
+        case 'dmg':
+          success = result.judge.success
+          break
+      }
+    })
+    return success ? '成功' : '失敗'
+  }
+
   // 結果ログ生成
-  private createMessages(request: ActionRequest): ReactNode[] {
+  private createMessages(request: ActionRequest, results: ActionResult[]): ReactNode[] {
     const actor = this.actor.name
     const messages = []
     switch (request.key) {
-      case 'attack':
-        messages.push(<>{`${actor} の攻撃!`}</>)
-        break
+      case 'attack': {
+        const target = request.target
+        results.forEach(result => {
+          switch (result.type) {
+            case 'attack':
+              messages.push(<>{`${actor} の ${this.actor.attack.name} による攻撃!`}</>)
+              messages.push(<>{`出目は ${result.judge.roll}、${this.getResultLabel(result.judge)}`}</>)
+              break
 
-      case 'move':
+            case 'defense':
+              const defenseTypeLabel = result.judge.type === 'parry' ? '武器による受け流し'
+                : result.judge.type === 'block' ? '盾による受け止め' : '回避'
+              messages.push(<>{`${target.name} は ${defenseTypeLabel} を試みた!`}</>)
+              messages.push(<>{`出目は ${result.judge.roll}、${this.getResultLabel(result.judge)}`}</>)
+              break
+
+            case 'dmg':
+              if (result.judge.roll < 1) messages.push(<>{`ダメージは ${target.name} の鎧によって完全に止められた...`}</>)
+              else if (!result.judge.critical) messages.push(<>{`${target.name} は ${result.judge.roll} 点のダメージを受けた!!`}</>)
+              else messages.push(<>{`${target.name} は ${result.judge.roll} 点のダメージを受けた!!!`}</>)
+              break
+          }
+        })
+        break
+      }
+      case 'move': {
         messages.push(<>{`${actor} は ${POSITION_LABELS[request.options.position]} へ移動した`}</>)
         break
-
-      default: // case 'wait':
+      }
+      default: { // case 'wait':
         messages.push(<>{`${actor} は 待機している`}</>)
         break
+      }
     }
     messages.push(<>&nbsp;</>)
     return messages
+  }
+
+  private getResultLabel(judge: Judge): string {
+    return judge.success && judge.critical ? 'クリティカル!!'
+      : judge.success && !judge.critical ? '成功!' : '失敗!'
   }
 }
