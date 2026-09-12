335051ba1695fd079b468ac11649e590444baa81
Taigo Ito <qwel.design@gmail.com>
2026-09-12 13:01:36 +0900

    feat: ターンとログの実装 (移動の実装にて検証)

--- src/domains/Combat/index.ts
+++ src/domains/Combat/index.ts
@@ -2,6 +2,8 @@
 
 import { type CombatUnitModel as UnitModel, CombatUnit as Unit } from './Unit'
 import { CombatFormation as Formation } from './Formation'
+import { CombatAction as Action } from './Action'
+import { CombatLog as Log } from './Log'
 
 // 全ての情報を集約・管理するクラス
 export class Combat {
@@ -9,14 +11,20 @@ export class Combat {
   public turnIndex: number // 行動順
   public units: Unit[]
   public formation: Formation | null
+  public action: Action | null
+  public logs: Log[]
+  public playLog: () => Promise<void> // Combat 本体から受け取り, ActionStore から呼び出す
 
-  constructor(models: UnitModel[]) {
+  constructor(models: UnitModel[], playLog: () => Promise<void>) {
     this.round = 1 // 1からカウント
     this.turnIndex = 0 // 開幕前は 0, 開幕と同時に 1 になる
     this.units = models.map((model, i) => {
-      return new Unit(model, i + 1) // combatIdは1からカウント
+      return new Unit(model, i + 1) // combatId は 1 からカウント
     })
     this.formation = null
+    this.action = null
+    this.logs = []
+    this.playLog = playLog
   }
 
   get actor() {
@@ -25,12 +33,35 @@ export class Combat {
 
   // 次のターンへ進む
   async nextTurn() {
+    this.advanceTurn()
+    await this.startTurn()
+    await this.waitForCommand()
+  }
+
+  // turnIndex / round を進める
+  private advanceTurn(): void {
     this.turnIndex++
     if (this.turnIndex > this.units.length) {
       this.round++
       this.turnIndex -= this.units.length
     }
+  }
+
+  // 新しいターンの開始処理
+  // formation, log, action 初期化
+  private async startTurn(): Promise<void> {
     this.formation = new Formation(this.actor, this.units)
+    this.logs.unshift(new Log(this.actor))
+    await this.playLog()
+    this.action = new Action(this)
+  }
+
+  // コマンド入力待機 → 状態更新 → 次のターンへ再帰
+  private async waitForCommand(): Promise<void> {
+    await this.action!.promise.then(() => {
+      this.debug()
+      this.nextTurn()
+    })
   }
 
   debug() {
