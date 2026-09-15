c27361adaedc4157b200d74b915e7ebb788241fa
Taigo Ito <qwel.design@gmail.com>
2026-09-15 22:40:36 +0900

    feat: ドキュメントのレンダリング機能を実装

--- src/parts/Docs.tsx
+++ src/parts/Docs.tsx
@@ -1,10 +1,39 @@
 // src/parts/Docs.tsx
 
+import { useLoaderData, Link } from 'react-router-dom'
+import { chapters } from '../docs/chapters'
+
 function Docs() {
+  const { data, content } = useLoaderData()
+
   return (
-    <>
-      
-    </>
+    <div className="row">
+      <article className="primary-col">
+        <h2>{data.Chapter}</h2>
+        <h3>{data.Section}</h3>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </article>
+      <aside className="secondary-col">
+        <h2>目次</h2>
+        <ol className="docs-nav">
+          <li className="docs-nav__chapter"><Link to="/docs/">序章</Link></li>
+          {chapters.map((chapter) => (
+            <li key={chapter.order} className="docs-nav__chapter">
+              <details>
+                <summary>{chapter.heading}</summary>
+                <ol className="docs-nav__sections">
+                  {chapter.sections.map((section) => (
+                    <li key={section.order} className="docs-nav__section">
+                      <Link to={`/docs/${chapter.order}-${section.order}`}>{section.heading}</Link>
+                    </li>
+                  ))}
+                </ol>
+              </details>
+            </li>
+          ))}
+        </ol>
+      </aside>
+    </div>
   )
 }
 
