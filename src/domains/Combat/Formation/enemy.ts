ee4a3b72a4fc069bb15f8cb689da54fa37cc8310
Taigo Ito <qwel.design@gmail.com>
2026-09-19 10:53:40 +0900

    feat: ゴブリン編成を追加

--- src/domains/Combat/Formation/enemy.ts
+++ src/domains/Combat/Formation/enemy.ts
@@ -3,10 +3,42 @@
 import { createSamples } from '../../Sample'
 import { SaveData } from '../../SaveData'
 import { type CombatUnitModel as UnitModel } from '../Unit'
+import { makeCombatEnemyModel, makeParamsByRank, makeGoblinFormation } from '../Mob'
 
+// Rank算出
+export function getRankFromCp(cp: number): number {
+  if (cp < 12) return 0
+  else if (cp < 16) return 1
+  else if (cp < 20) return 2
+  else return 3
+}
+
+// シード値 0～15 を生成 (ただし, player側と同じシード値は取らない)
 export function enemySetup(saveData: SaveData, seed: number = 0): { models: UnitModel[], rewardCp: number, rewardGold: number } {
   // セーブデータの読み込み
   const cp = saveData.loadPoints()
+
+  // ゴブリン編成とエンカウントする確率
+  // cp: 10 --> 100%
+  // cp: 11 --> 66.6%
+  // cp: 12 --> 33.3% (以降固定)
+  const rate = 1 - (Math.min(cp, 12) - 10) / 3
+  
+  // ゴブリン編成
+  if (Math.random() < rate) {
+    seed %= 4
+    const formation = makeGoblinFormation(seed)
+    console.log(formation)
+    const rank = getRankFromCp(cp)
+    const models = formation.map(member => {
+      const params = makeParamsByRank(member.params, rank)
+      const { name, equips } = member
+      return makeCombatEnemyModel(name, params, equips)
+    })
+
+    return { models, rewardCp: 1, rewardGold: 50 }
+  }
+
   // サンプル編成
   const models = createSamples(4, cp, 4, seed).units
     .map(unit => unit.combatUnitModel)
---
+++ src/domains/Combat/Mob/index.ts
@@ -0,0 +1,77 @@
+// src/domains/Combat/Mob/index.ts
+
+import { type WeaponKey, type ShieldKey, type ArmorKey, Equipments } from '../../Character'
+import { type CombatUnitModel as UnitModel } from '../Unit'
+import { type SpellElement } from '../Spells'
+import { type TacticTypeKey, type TemperTypeKey } from '../AI'
+import { makeGoblinFormation } from './Goblin'
+
+// 敵生成時の基本パラメータ
+export type EnemyParams = {
+  maxHp: number // 最大Hp
+  level: number // 技能値
+  dmgMod: number // ダメージ修正
+  dmgBuff: number // StatusBuff の初期値
+  ev: number // 回避値「よけ」
+  evBuff: number // StatusBuff の初期値
+  pre: number // 身体的な抵抗値
+  mre: number // 精神的な抵抗値
+}
+
+// 敵生成時の装備
+export type EnemyEquips = [
+  weapon: WeaponKey, // 武器
+  shield: ShieldKey, // 盾
+  armor: ArmorKey, // 服・鎧
+]
+
+export type EnemyDef = {
+  name: string
+  params: EnemyParams
+  equips: EnemyEquips
+  elements?: SpellElement[]
+  tacticType?: TacticTypeKey
+  temperType?: TemperTypeKey
+}
+
+// 名前, 基本パラメータ, 装備を引数に取って, 敵を生成
+export function makeCombatEnemyModel(name: string, params: EnemyParams, equips: EnemyEquips, elements: SpellElement[] = []): UnitModel {
+  return {
+    name,
+    maxHp: params.maxHp,
+    level: params.level,
+    dmgMod: params.dmgMod,
+    dmgBuff: params.dmgBuff,
+    ev: params.ev,
+    evBuff: params.evBuff,
+    pre: params.pre,
+    mre: params.mre,
+    equipments: new Equipments(...equips),
+    elements: {
+      blue: elements.includes('blue') ? params.level : 0,
+      red: elements.includes('red') ? params.level : 0,
+      green: elements.includes('green') ? params.level : 0
+    }
+  }
+}
+
+// ランク (0～3) に応じたパラメータバフ
+// パラメータは乱数で少しの程度幅を持たせる
+export function makeParamsByRank(params: EnemyParams, rank: number): EnemyParams {
+  const r1 = Math.floor(Math.random() * 2) // Hp, pre
+  const r2 = Math.floor(Math.random() * 2) // level, ev
+  const r3 = Math.floor(Math.random() * 2) // dmgMod
+
+  return {
+    maxHp: params.maxHp + (rank + r1) * 2,
+    level: params.level + (rank + r2),
+    dmgMod: params.dmgMod + Math.floor((rank + r3) / 2),
+    dmgBuff: params.dmgBuff + (rank + r3) % 2,
+    ev: params.ev + Math.floor((rank + r2) / 2),
+    evBuff: params.evBuff + (rank + r2) % 2,
+    pre: params.pre + Math.floor((rank + r1) / 2),
+    mre: params.mre + Math.floor(rank / 2)
+  }
+}
+
+export { makeGoblinFormation }
